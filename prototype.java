7f7faf1ca398b630711f5d34e5bce52b6b75dfab
William Gu <wg2401@columbia.edu>
2025-07-06 01:52:13 -0400

    initial java function written, some backtracking errors

--- prototype.java
+++ prototype.java
@@ -7,14 +7,100 @@ public class prototype
     }
 
 
-    public static String[] findWay(int[] nums, int[] used, String[] operationString)
+    public static String[] findWay(double[] nums, int[] used, String[] operationStrings, int curStep)
     {
+        if (curStep == 3)
+        {
+            double check = 0;
+            for (int i = 0; i < nums.length; i++)
+            {
+                if (used[i] == 0)
+                {
+                    check = nums[i];
+                    break;
+                }
+            }
+
+            // check for approx val to account for floating pt precision error
+            if (Math.abs(check - 24.0) < 1e-6)
+            {
+                return operationStrings;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        double tempI, tempJ;
+        String[] potAns;
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (used[i] == 0)
+            {
+                //mark i as used
+                used[i] = 1;
+                tempI = nums[i];
+
+                for (int j = 0; j < nums.length; j++)
+                {
+                    //check for used (covers i = j case)
+                    if (used[j] == 0)
+                    {
+                        tempJ = nums[j];
+                        
+                        //add nums[i] and nums[j] and recurse
+                        nums[j] = tempI + tempJ;
+                        operationStrings[curStep] = tempI + "+" + tempJ;
+                        potAns = findWay(nums, used, operationStrings, curStep + 1);
+                        if (potAns!=null)
+                        {
+                            return potAns;
+                        }
+
+                        //multiply and recurse
+                        nums[j] = tempI * tempJ;
+                        operationStrings[curStep] = tempI + "x" + tempJ;
+                        potAns = findWay(nums, used, operationStrings, curStep + 1);
+                        if (potAns!=null)
+                        {
+                            return potAns;
+                        }
+
+                        //nums[i]/nums[j] and recurse
+                        if (tempJ != 0)
+                        {
+                            nums[j] = tempI/tempJ;
+                            operationStrings[curStep] = tempI + "/" + tempJ;
+                            potAns = findWay(nums, used, operationStrings, curStep + 1);
+                            if (potAns!=null)
+                            {
+                                return potAns;
+                            }
+                        }
+
+                        //nums[i] - nums[j] and recurse
+                        nums[j] = tempI - tempJ;
+                        operationStrings[curStep] = tempI + "-" + tempJ;
+                        potAns = findWay(nums, used, operationStrings, curStep + 1);
+                        if (potAns!=null)
+                        {
+                            return potAns;
+                        }
+
+                        //todo: do the reverse operands of sub and div
+
+
+                        //backtrack:
+                        nums[j] = tempJ;
+                    }
+                }
+
+                used[i] = 0;
+            }
+        }
         
-
-
-
-
-
 
         return null;
     }
