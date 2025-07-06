442440507185d1720db735554619db2036c0dd17
William Gu <wg2401@columbia.edu>
2025-07-06 00:04:17 -0400

    initial commit

---
+++ prototype.java
@@ -0,0 +1,21 @@
+
+public class prototype 
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+
+    public static String[] findWay(int[] nums, int[] used, String[] operationString)
+    {
+        
+
+
+
+
+
+
+        return null;
+    }
+}
